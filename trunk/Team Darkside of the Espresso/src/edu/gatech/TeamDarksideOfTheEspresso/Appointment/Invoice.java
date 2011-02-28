b8cac318dde7a01356d68a9394c23fe904e647c6
fpnewton90 <fpnewton90@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-02-28 02:12:10 +0000

    Initial release of partial class templates.

---
+++ trunk/Team Darkside of the Espresso/src/edu/gatech/TeamDarksideOfTheEspresso/Appointment/Invoice.java
@@ -0,0 +1,38 @@
+package edu.gatech.TeamDarksideOfTheEspresso.Appointment;
+
+
+public class Invoice
+{
+	Appointment	Appointment;
+	Bill		Bill;
+	
+	
+	public Appointment getAppointment()
+	{
+		return Appointment;
+	}
+	
+	
+	public Bill getBill()
+	{
+		return Bill;
+	}
+	
+	
+	public void setAppointment(Appointment Appointment)
+	{
+		this.Appointment = Appointment;
+	}
+	
+	
+	public void setBill(Bill Bill)
+	{
+		this.Bill = Bill;
+	}
+	
+	
+	public void setPrintInvoice()
+	{
+		// TODO Implement Method
+	}
+}
---
+++ trunk/Team Darkside of the Espresso/src/edu/gatech/TeamDarksideOfTheEspresso/Record/Prescription.java
@@ -0,0 +1,32 @@
+package edu.gatech.TeamDarksideOfTheEspresso.Record;
+
+
+public class Prescription
+{
+	private String	Info;
+	private String	Price;
+	
+	
+	public String getInfo()
+	{
+		return Info;
+	}
+	
+	
+	public String getPrice()
+	{
+		return Price;
+	}
+	
+	
+	public void setInfo(String Info)
+	{
+		this.Info = Info;
+	}
+	
+	
+	public void setPrice(String Price)
+	{
+		this.Price = Price;
+	}
+}
