2ede27dd90c95d6d0b4ebf0319debcc6fc0e5737
asoundbassist@gmail.com <asoundbassist@gmail.com@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-03-10 19:53:16 +0000

    Made constructor for Invoice.java
    Implemented pseudocode for all Appointment methods. Still need to get some things squared away with the cooperation between Doctor.java and Appointment.java
    
    Completed constructor for PatientInfo.java. Removed WeightHistory field as that is covered in the HealthHistory class. Will add a HealthHistory field soon.

--- Team Darkside of the Espresso/src/edu/gatech/TeamDarksideOfTheEspresso/Appointment/Appointment.java
+++ Team Darkside of the Espresso/src/edu/gatech/TeamDarksideOfTheEspresso/Appointment/Appointment.java
@@ -33,6 +33,61 @@ public class Appointment
 		this.Symptoms=Symptoms;
 	}
 	
+	/**
+	 * Schedules a doctor at a specific date if he/she is available
+	 * 
+	 * @param doc The desired doctor
+	 * @param date The desired date
+	 * @param time The desired time
+	 */
+	public void scheduleDoctor(Doctor doc, Date date, Time time){
+		//TODO This method depends heavily on doctor attributes
+		//		that determine availability as well as an attribute
+		//		that stores dates during which the doctor has
+		//		scheduled appointments. This method,
+		//		therefore, is commented out and in high-level
+		//		pseudocode until this is fixed
+		
+		/**
+		 *  if(!doc.checkAvailability(date, time))
+		 *  	return;
+		 *  
+		 *  else{
+		 *  	remove Date from available dates list
+		 *  	insert Date into unavailable dates list
+		 *  }
+		 */
+	}
+	
+	/**
+	 * Checks whether a doctor is available at the desired date
+	 * and time
+	 * 
+	 * @param doc The desired doctor
+	 * @param date The desired date
+	 * @param time The desired time
+	 * @return True if the doctor is available at that time,
+	 * 			false otherwise
+	 */
+	public boolean checkAvailability(Date date, Time time){
+		//TODO I believe this should be in the Doctor class. Therefore,
+		//		the method has been commented out for now.
+		
+		/**
+		 * check which date list is smaller (available or unavailable)
+		 * 
+		 * if unavailable is smaller, comb through for the date
+		 * if the date is there, return false
+		 * 	else return true
+		 * 
+		 * else if available is smaller, comb through for the date
+		 * if the date is there, return true
+		 * 	else return false
+		 */
+		
+		return false;
+	}
+	
 	public Date getDate()
 	{
 		return Date;
