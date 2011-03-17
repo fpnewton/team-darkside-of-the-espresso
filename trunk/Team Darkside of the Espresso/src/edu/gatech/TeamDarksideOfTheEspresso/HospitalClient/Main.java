076a0f68ade6dde318d073d10882fd33c0a419dc
fpnewton90 <fpnewton90@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-03-17 18:39:18 +0000

    Fix repo errors

---
+++ trunk/Team Darkside of the Espresso/src/edu/gatech/TeamDarksideOfTheEspresso/HospitalClient/Main.java
@@ -0,0 +1,52 @@
+/**
+ * Hospital Client Application Entry Point
+ * 
+ * @author	Fraser P. Newton
+ * @date	March 4, 2011
+ */
+
+package edu.gatech.TeamDarksideOfTheEspresso.HospitalClient;
+
+
+import java.awt.EventQueue;
+
+import edu.gatech.TeamDarksideOfTheEspresso.UI.LoginWindow;
+
+
+public class Main
+{	
+	/**
+	 * Hospital Client application entry point
+	 * 
+	 * @param args	Application arguments
+	 * @throws		IOException 
+	 */
+	public static void main(String[] args)
+	{
+		ClientThread ct = new ClientThread();
+		ct.start();
+		
+		EventQueue.invokeLater(new Runnable()
+		{
+			public void run()
+			{
+				try
+				{
+					LoginWindow window = new LoginWindow();
+					
+					window.getFrmLoginWindow().setVisible(true);
+				}
+				catch (Exception e)
+				{
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+	
+	
+	public String toString()
+	{
+		return "";
+	}
+}
