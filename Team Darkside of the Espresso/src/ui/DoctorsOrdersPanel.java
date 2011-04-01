e3701161614c52a55222872c16b94891b85bff38
ptynan123 <ptynan123@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-04-01 16:59:46 +0000

    Panel to create Doctors Orders for a particular Appointment.

---
+++ Team Darkside of the Espresso/src/ui/DoctorsOrdersPanel.java
@@ -0,0 +1,63 @@
+package ui;
+
+import javax.swing.JPanel;
+import javax.swing.JEditorPane;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.SystemColor;
+import javax.swing.SwingConstants;
+import javax.swing.JSeparator;
+import javax.swing.JTextArea;
+
+public class DoctorsOrdersPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public DoctorsOrdersPanel() {
+		setLayout(null);
+		
+		JEditorPane dtrpnEnterOrdersHere = new JEditorPane();
+		dtrpnEnterOrdersHere.setText("Enter Orders Here");
+		dtrpnEnterOrdersHere.setBounds(200, 117, 189, 70);
+		add(dtrpnEnterOrdersHere);
+		
+		JButton btnAddPrescription = new JButton("Add Prescription...");
+		btnAddPrescription.setBounds(38, 58, 150, 29);
+		add(btnAddPrescription);
+		
+		JButton btnAddVaccine = new JButton("Add Vaccine...");
+		btnAddVaccine.setBounds(38, 102, 150, 29);
+		add(btnAddVaccine);
+		
+		JButton btnAddLabwork = new JButton("Add LabWork...");
+		btnAddLabwork.setBounds(38, 143, 150, 29);
+		add(btnAddLabwork);
+		
+		JEditorPane dtrpnAddAnyOther = new JEditorPane();
+		dtrpnAddAnyOther.setBackground(SystemColor.textHighlight);
+		dtrpnAddAnyOther.setText("Add any other instructions in the box below.");
+		dtrpnAddAnyOther.setBounds(200, 58, 189, 42);
+		add(dtrpnAddAnyOther);
+		
+		JLabel lblDoctorsOrders = new JLabel("Doctor's Orders");
+		lblDoctorsOrders.setHorizontalAlignment(SwingConstants.CENTER);
+		lblDoctorsOrders.setBounds(6, 6, 438, 16);
+		add(lblDoctorsOrders);
+		
+		JSeparator separator = new JSeparator();
+		separator.setBounds(6, 34, 438, 12);
+		add(separator);
+		
+		JTextArea txtrAppointmentSymptoms = new JTextArea();
+		txtrAppointmentSymptoms.setText("Appointment Symptoms");
+		txtrAppointmentSymptoms.setBounds(6, 227, 438, 50);
+		add(txtrAppointmentSymptoms);
+		
+		JLabel lblCurrentAppointmentSymptoms = new JLabel("Current Appointment Symptoms");
+		lblCurrentAppointmentSymptoms.setHorizontalAlignment(SwingConstants.CENTER);
+		lblCurrentAppointmentSymptoms.setBounds(6, 199, 438, 16);
+		add(lblCurrentAppointmentSymptoms);
+
+	}
+}
