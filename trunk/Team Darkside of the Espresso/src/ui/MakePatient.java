e28997bbfa99d4d92a2976a7b6543590f8cea0bf
fpnewton90 <fpnewton90@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-04-07 17:30:21 +0000

    Updated GUI

--- trunk/Team Darkside of the Espresso/src/ui/MakePatient.java
+++ trunk/Team Darkside of the Espresso/src/ui/MakePatient.java
@@ -1,157 +1,322 @@
+// $codepro.audit.disable numericLiterals
+
+/*
+ * The MakeSystemAdmin Class.
+ */
+
 package ui;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JPasswordField;
 import javax.swing.JRadioButton;
 import javax.swing.JTextField;
+import javax.swing.SwingConstants;
 
+import log.SystemLog;
+import users.GenderType;
+import users.Patient;
+import users.PatientInfo;
+import users.User;
+import users.UserInfo;
+import client.Main;
+import database.Crypto;
+import database.SqlDatabase;
+
+/**
+ * The MakeSystemAdmin Class.
+ * 
+ * @author Jonathan Cherry
+ * @version 2.0.0
+ */
 public class MakePatient extends JPanel {
-	private JTextField textField;
-	private final ButtonGroup genderGroup = new ButtonGroup();
-	
-	/** The Constant WIDTH. */
+
+    /** The gender group. */
+    private final ButtonGroup genderGroup = new ButtonGroup();
+
+    /** The Constant WIDTH. */
     private static final int WIDTH = 450;
 
     /** The Constant HEIGHT. */
     private static final int HEIGHT = 300 + 25;
-    
-    private static final String TITLE = "Make Patient";
 
-	/**
-	 * Create the panel.
-	 */
-	public MakePatient() {
-		setLayout(null);
-		
-		JLabel nameL = new JLabel("Name:");
-		nameL.setBounds(151, 35, 40, 14);
-		add(nameL);
-		
-		JLabel pwordL = new JLabel("Password:");
-		pwordL.setBounds(133, 76, 66, 14);
-		add(pwordL);
-		
-		JLabel addressL = new JLabel("Address:");
-		addressL.setBounds(141, 98, 58, 14);
-		add(addressL);
-		
-		JLabel cityszL = new JLabel("City/State/Zip:");
-		cityszL.setBounds(102, 118, 97, 14);
-		add(cityszL);
-		
-		JLabel phoneL = new JLabel("Phone Number:");
-		phoneL.setBounds(102, 144, 97, 14);
-		add(phoneL);
-		
-		JLabel socialL = new JLabel("Social Security Number:");
-		socialL.setBounds(50, 168, 149, 14);
-		add(socialL);
-		
-		JLabel emailL = new JLabel("Email Address:");
-		emailL.setBounds(102, 191, 97, 14);
-		add(emailL);
-		
-		JLabel birthL = new JLabel("Birth Date (MM/DD/YYYY):");
-		birthL.setBounds(33, 214, 166, 14);
-		add(birthL);
-		
-		JLabel genderL = new JLabel("Gender:");
-		genderL.setBounds(99, 248, 66, 14);
-		add(genderL);
-		
-		JTextField nameE = new JTextField();
-		nameE.setBounds(211, 32, 130, 20);
-		add(nameE);
-		nameE.setColumns(10);
-		
-		JTextField pwordE = new JTextField();
-		pwordE.setColumns(10);
-		pwordE.setBounds(211, 73, 130, 20);
-		add(pwordE);
-		
-		JTextField addressE = new JTextField();
-		addressE.setColumns(10);
-		addressE.setBounds(211, 95, 130, 20);
-		add(addressE);
-		
-		JTextField cityszE = new JTextField();
-		cityszE.setColumns(10);
-		cityszE.setBounds(211, 118, 130, 20);
-		add(cityszE);
-		
-		JTextField phoneE = new JTextField();
-		phoneE.setColumns(10);
-		phoneE.setBounds(211, 141, 130, 20);
-		add(phoneE);
-		
-		JTextField socialE = new JTextField();
-		socialE.setColumns(10);
-		socialE.setBounds(211, 165, 130, 20);
-		add(socialE);
-		
-		JTextField emailE = new JTextField();
-		emailE.setColumns(10);
-		emailE.setBounds(211, 188, 130, 20);
-		add(emailE);
-		
-		JTextField birthE = new JTextField();
-		birthE.setColumns(10);
-		birthE.setBounds(211, 211, 130, 20);
-		add(birthE);
-		
-		JRadioButton maleRadB = new JRadioButton("Male");
-		genderGroup.add(maleRadB);
-		maleRadB.setBounds(177, 243, 76, 23);
-		add(maleRadB);
-		maleRadB.setSelected(true);
-		
-		JRadioButton femaleRadB = new JRadioButton("Female");
-		genderGroup.add(femaleRadB);
-		femaleRadB.setBounds(265, 243, 118, 23);
-		add(femaleRadB);
-		
-		JCheckBox activeCB = new JCheckBox("Active?");
-		activeCB.setBounds(158, 269, 97, 23);
-		add(activeCB);
-		
-		JButton btnSave = new JButton("Save");
-		btnSave.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				
-			}
-		});
-		btnSave.setBounds(265, 271, 76, 23);
-		add(btnSave);
-		
-		JTextField unameE = new JTextField();
-		unameE.setBounds(211, 52, 130, 20);
-		add(unameE);
-		unameE.setColumns(10);
-		
-		JLabel lblUsername = new JLabel("Username:");
-		lblUsername.setBounds(125, 54, 66, 16);
-		add(lblUsername);
+    /** The Constant TITLE. */
+    private static final String TITLE = "Make System Admin";
 
-		
-		/* find function... */
+    /** The name text box. */
+    private JTextField txtName;
+
+    /** The password text box. */
+    private JPasswordField txtPassword;
+
+    /** The address text box. */
+    private JTextField txtAddress;
+
+    /** The city, state & ZIP text box. */
+    private JTextField txtCityStateZip;
+
+    /** The phone number text box. */
+    private JTextField txtPhoneNum;
+
+    /** The social security number text box. */
+    private JTextField txtSSN;
+
+    /** The email address text box. */
+    private JTextField txtEmailAddress;
+
+    /** The birth date text box. */
+    private JTextField txtBirthDate;
+
+    /** The username text box. */
+    private JTextField txtusername;
+
+    /** The active check box. */
+    private JCheckBox chkActive;
+
+    /** The male radio button. */
+    private JRadioButton rdoMale;
+
+    /** The female radio button. */
+    private JRadioButton rdoFemale;
+
+    /**
+     * Create the panel.
+     */
+    public MakePatient() {
+	initialize();
+    }
+
+    /**
+     * Initialize the panel.
+     */
+    private void initialize() {
+	setLayout(null);
+
+	final JLabel lblName = new JLabel("Name:");
+	lblName.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblName.setBounds(12, 35, 187, 14);
+	add(lblName);
+
+	final JLabel lblPassword = new JLabel("Password:");
+	lblPassword.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblPassword.setBounds(12, 76, 187, 14);
+	add(lblPassword);
+
+	final JLabel lblAddress = new JLabel("Address:");
+	lblAddress.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblAddress.setBounds(12, 98, 187, 14);
+	add(lblAddress);
+
+	final JLabel lblCityStateZip = new JLabel("City/State/Zip:");
+	lblCityStateZip.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblCityStateZip.setBounds(12, 118, 187, 14);
+	add(lblCityStateZip);
+
+	final JLabel lblPhoneNum = new JLabel("Phone Number:");
+	lblPhoneNum.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblPhoneNum.setBounds(12, 144, 187, 14);
+	add(lblPhoneNum);
+
+	final JLabel lblSSN = new JLabel("Social Security Number:");
+	lblSSN.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblSSN.setBounds(12, 168, 187, 14);
+	add(lblSSN);
+
+	final JLabel lblEmail = new JLabel("Email Address:");
+	lblEmail.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblEmail.setBounds(12, 191, 187, 14);
+	add(lblEmail);
+
+	final JLabel lblBirthDate = new JLabel("Birth Date (MM/DD/YYYY):");
+	lblBirthDate.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblBirthDate.setBounds(12, 214, 187, 14);
+	add(lblBirthDate);
+
+	final JLabel lblGender = new JLabel("Gender:");
+	lblGender.setBounds(99, 248, 66, 14);
+	add(lblGender);
+
+	txtName = new JTextField();
+	txtName.setBounds(211, 32, 227, 20);
+	add(txtName);
+	txtName.setColumns(10);
+
+	txtPassword = new JPasswordField();
+	txtPassword.setColumns(10);
+	txtPassword.setBounds(211, 73, 227, 20);
+	add(txtPassword);
+
+	txtAddress = new JTextField();
+	txtAddress.setColumns(10);
+	txtAddress.setBounds(211, 95, 227, 20);
+	add(txtAddress);
+
+	txtCityStateZip = new JTextField();
+	txtCityStateZip.setColumns(10);
+	txtCityStateZip.setBounds(211, 118, 227, 20);
+	add(txtCityStateZip);
+
+	txtPhoneNum = new JTextField();
+	txtPhoneNum.setColumns(10);
+	txtPhoneNum.setBounds(211, 141, 227, 20);
+	add(txtPhoneNum);
+
+	txtSSN = new JTextField();
+	txtSSN.setColumns(10);
+	txtSSN.setBounds(211, 165, 227, 20);
+	add(txtSSN);
+
+	txtEmailAddress = new JTextField();
+	txtEmailAddress.setColumns(10);
+	txtEmailAddress.setBounds(211, 188, 227, 20);
+	add(txtEmailAddress);
+
+	txtBirthDate = new JTextField();
+	txtBirthDate.setColumns(10);
+	txtBirthDate.setBounds(211, 211, 227, 20);
+	add(txtBirthDate);
+
+	rdoMale = new JRadioButton("Male");
+	genderGroup.add(rdoMale);
+	rdoMale.setBounds(177, 243, 76, 23);
+	add(rdoMale);
+	rdoMale.setSelected(true);
+
+	rdoFemale = new JRadioButton("Female");
+	genderGroup.add(rdoFemale);
+	rdoFemale.setBounds(265, 243, 118, 23);
+	add(rdoFemale);
+
+	chkActive = new JCheckBox("Active?");
+	chkActive.setBounds(158, 269, 97, 23);
+	add(chkActive);
+
+	final JButton btnSave = new JButton("Save");
+	btnSave.addMouseListener(new MouseAdapter() {
+	    @Override
+	    public void mouseClicked(MouseEvent arg0) {
+		createUser();
+	    }
+	});
+	btnSave.setBounds(265, 271, 76, 23);
+	add(btnSave);
+
+	txtusername = new JTextField();
+	txtusername.setBounds(211, 52, 227, 20);
+	add(txtusername);
+	txtusername.setColumns(10);
+
+	final JLabel lblUsername = new JLabel("Username:");
+	lblUsername.setHorizontalAlignment(SwingConstants.RIGHT);
+	lblUsername.setBounds(12, 54, 187, 16);
+	add(lblUsername);
+
+	JButton btnCancel = new JButton("Cancel");
+	btnCancel.addMouseListener(new MouseAdapter() {
+	    @Override
+	    public void mouseClicked(MouseEvent e) {
+		final MakeUser makeUserPanel = new MakeUser();
+
+		Main.getApplicationWindow().setFrame(makeUserPanel,
+			makeUserPanel.getTitle(), makeUserPanel.getWidth(),
+			makeUserPanel.getHeight());
+	    }
+	});
+	btnCancel.setBounds(353, 271, 85, 23);
+	add(btnCancel);
+    }
+
+    /**
+     * Gets the title.
+     * 
+     * @return The title of the panel
+     */
+    public String getTitle() {
+	return TITLE;
+    }
+
+    /**
+     * Gets the width of the panel.
+     * 
+     * @return The width of the panel
+     */
+    public int getWidth() {
+	return WIDTH;
+    }
+
+    /**
+     * Gets the height of the panel.
+     * 
+     * @return The height of the panel
+     */
+    public int getHeight() {
+	return HEIGHT;
+    }
+
+    /**
+     * Creates the user.
+     */
+    private void createUser() {
+	final char[] password = txtPassword.getPassword();
+	final StringBuilder strPassword = new StringBuilder();
+	final SqlDatabase db = Main.getDatabaseObject();
+	UserInfo userInfo = null;
+	User user = null;
+	PatientInfo patientInfo = null;
+	final DateFormat dateFormatter = new SimpleDateFormat("MM/DD/yyyy");
+	Date birthDate = null;
+
+	try {
+	    birthDate = dateFormatter.parse(txtBirthDate.getText());
+	} catch (ParseException e) {
+	    if (!SystemLog.LogMessage(e.getMessage(), Level.SEVERE)) {
+		e.printStackTrace();
+	    }
+	} finally {
+	    for (char c : password) {
+		strPassword.append(c);
+	    }
+
+	    userInfo = new UserInfo(txtName.getText(), txtAddress.getText() + "\n"
+		    + txtCityStateZip.getText(), txtPhoneNum.getText(), txtSSN.getText(),
+		    txtEmailAddress.getText(), birthDate);
+
+	    // TODO Change patient info
+	    patientInfo = new PatientInfo(0, null, null, null, null);
+
+	    user = new Patient(txtusername.getText(), Crypto.getSha1Hash(strPassword
+		    .toString()), (rdoMale.isSelected() ? GenderType.Male
+		    : GenderType.Female), userInfo, patientInfo);
+
+	    if (db.canInsertUser(user)) {
+		if (!SystemLog.LogMessage("User " + userInfo.getName()
+			+ " created successfully.", Level.INFO)) {
+		    System.out.println("Error: Could not log message \"User "
+			    + userInfo.getName() + " created successfully.\"");
+		} else {
+		    if (!SystemLog.LogMessage("Error: User " + userInfo.getName()
+			    + " could not be created.", Level.WARNING)) {
+			System.out.println("Error: Could not log message \"Error: User "
+				+ userInfo.getName() + " could not be created.\"");
+		    }
+		}
+	    }
 	}
-	
-	public String getTitle() {
-		return TITLE;
-	}
-	    
-	public int getWidth() {
-		return WIDTH;
-	}
-	    
-	public int getHeight() {
-		return HEIGHT;
-	}
+
+	final MakeUser makeUserPanel = new MakeUser();
+
+	Main.getApplicationWindow().setFrame(makeUserPanel, makeUserPanel.getTitle(),
+		makeUserPanel.getWidth(), makeUserPanel.getHeight());
+    }
 }
--- trunk/Team Darkside of the Espresso/src/users/Patient.java
+++ trunk/Team Darkside of the Espresso/src/users/Patient.java
@@ -7,7 +7,6 @@
 
 package users;
 
-
 import java.sql.Time;
 import java.util.ArrayList;
 import java.util.Date;
@@ -20,210 +19,179 @@ import record.MedicalHistory;
 
 import appointment.Appointment;
 
+public class Patient extends User {
 
+    /** The my patient info. */
+    private PatientInfo myPatientInfo;
 
-public class Patient extends User
-{
+    /** The Appointment history. */
+    private ArrayList<Appointment> AppointmentHistory;
 
-	/** The my patient info. */
-	private PatientInfo				myPatientInfo;
+    /** The my med history. */
+    private MedicalHistory myMedHistory;
 
-	/** The Appointment history. */
-	private ArrayList<Appointment>	AppointmentHistory;
+    /** The my health history. */
+    private HealthHistory myHealthHistory;
 
-	/** The my med history. */
-	private MedicalHistory			myMedHistory;
+    /**
+     * Instantiates a new patient.
+     * 
+     * @param Uname
+     *            the uname
+     * @param Pword
+     *            the pword
+     * @param Gend
+     *            the gend
+     * @param Info
+     *            the info
+     * @param PatInfo
+     *            the pat info
+     */
+    // public Patient(){
+    // super();
+    // }
 
-	/** The my health history. */
-	private HealthHistory			myHealthHistory;
+    /**
+     * Instantiates a new patient.
+     * 
+     * @param Uname
+     *            the uname
+     * @param Pword
+     *            the pword
+     * @param Gend
+     *            the gend
+     * @param Info
+     *            the info
+     * @param PatInfo
+     *            the pat info
+     */
+    public Patient(String Uname, String Pword, GenderType Gend, UserInfo Info,
+	    PatientInfo PatInfo) {
+	super(Uname, Pword, Gend, Info);
+	this.myPatientInfo = PatInfo;
+    }
 
+    /**
+     * Gets the patient info.
+     * 
+     * @return the patient info
+     */
+    public PatientInfo getPatientInfo() {
+	return myPatientInfo;
+    }
 
-	/**
-	 * Instantiates a new patient.
-	 * 
-	 * @param Uname
-	 *            the uname
-	 * @param Pword
-	 *            the pword
-	 * @param Gend
-	 *            the gend
-	 * @param Info
-	 *            the info
-	 * @param PatInfo
-	 *            the pat info
-	 */
-	// public Patient(){
-	// super();
-	// }
+    // TODO Add Get Methods/Set Methods
 
-	/**
-	 * Instantiates a new patient.
-	 * 
-	 * @param Uname
-	 *            the uname
-	 * @param Pword
-	 *            the pword
-	 * @param Gend
-	 *            the gend
-	 * @param Info
-	 *            the info
-	 * @param PatInfo
-	 *            the pat info
-	 */
-	public Patient(String Uname, String Pword, GenderType Gend, UserInfo Info, PatientInfo PatInfo)
-	{
-		super(Uname, Pword, Gend, Info);
-		this.myPatientInfo = PatInfo;
+    /**
+     * Schedule appointment.
+     * 
+     * @param date
+     *            the date
+     * @param time
+     *            the time
+     * @param symptoms
+     *            the symptoms
+     * @param PreferredDoctor
+     *            the preferred doctor
+     */
+    public void ScheduleAppointment(Date date, Time time, String symptoms,
+	    Doctor PreferredDoctor) {
+	try {
+	    Appointment Appt = new Appointment(this, date, PreferredDoctor, symptoms);
+	    if (PreferredDoctor.checkAvailability(time)) {
+		Appt.scheduleDoctor(PreferredDoctor, time);
+	    }
+	} catch (Exception e) {
+	    SystemLog.LogMessage(e.getMessage(), Level.SEVERE);
 	}
+    }
 
+    /**
+     * Sets the patient info.
+     * 
+     * @param PatInfo
+     *            the new patient info
+     */
+    public void setPatientInfo(PatientInfo PatInfo) {
+	this.myPatientInfo = PatInfo;
+    }
 
-	/**
-	 * Gets the patient info.
-	 * 
-	 * @return the patient info
-	 */
-	public PatientInfo getPatientInfo()
-	{
-		return myPatientInfo;
-	}
+    /**
+     * View medical history.
+     */
+    public void ViewMedicalHistory() {
+	myMedHistory.toString();
+    }
 
+    /**
+     * Sets the medical history.
+     * 
+     * @param MHistory
+     *            the new medical history
+     */
+    public void setMedicalHistory(MedicalHistory MHistory) {
+	this.myMedHistory = MHistory;
+    }
 
-	// TODO Add Get Methods/Set Methods
+    /**
+     * Gets the medical history.
+     * 
+     * @return the medical history
+     */
+    public MedicalHistory getMedicalHistory() {
+	return this.myMedHistory;
+    }
 
-	/**
-	 * Schedule appointment.
-	 * 
-	 * @param date
-	 *            the date
-	 * @param time
-	 *            the time
-	 * @param symptoms
-	 *            the symptoms
-	 * @param PreferredDoctor
-	 *            the preferred doctor
-	 */
-	public void ScheduleAppointment(Date date, Time time, String symptoms, Doctor PreferredDoctor)
-	{
-		try
-		{
-			Appointment Appt = new Appointment(this, date, PreferredDoctor, symptoms);
-			if (PreferredDoctor.checkAvailability(time))
-			{
-				Appt.scheduleDoctor(PreferredDoctor, time);
-			}
-		}
-		catch (Exception e)
-		{
-			SystemLog.LogMessage(e.getMessage(), Level.SEVERE);
-		}
-	}
+    /**
+     * Sets the health history.
+     * 
+     * @param HHistory
+     *            the new health history
+     */
+    public void setHealthHistory(HealthHistory HHistory) {
+	this.myHealthHistory = HHistory;
+    }
 
+    /**
+     * View health history.
+     */
+    public void ViewHealthHistory() {
+	// TODO Implement Method
+    }
 
-	/**
-	 * Sets the patient info.
-	 * 
-	 * @param PatInfo
-	 *            the new patient info
-	 */
-	public void setPatientInfo(PatientInfo PatInfo)
-	{
-		this.myPatientInfo = PatInfo;
-	}
+    /**
+     * View invoice.
+     */
+    public void ViewInvoice() {
+	// TODO Implement Method
+    }
 
+    /**
+     * Gets the health history.
+     * 
+     * @return the health history
+     */
+    public HealthHistory getHealthHistory() {
+	return myHealthHistory;
+    }
 
-	/**
-	 * View medical history.
-	 */
-	public void ViewMedicalHistory()
-	{
-		myMedHistory.toString();
-	}
+    /**
+     * Sets the appointment history.
+     * 
+     * @param appointmentHistory
+     *            the new appointment history
+     */
+    public void setAppointmentHistory(ArrayList<Appointment> appointmentHistory) {
+	AppointmentHistory = appointmentHistory;
+    }
 
+    /**
+     * Gets the appointment history.
+     * 
+     * @return the appointment history
+     */
+    public ArrayList<Appointment> getAppointmentHistory() {
+	return AppointmentHistory;
+    }
 
-	/**
-	 * Sets the medical history.
-	 * 
-	 * @param MHistory
-	 *            the new medical history
-	 */
-	public void setMedicalHistory(MedicalHistory MHistory)
-	{
-		this.myMedHistory = MHistory;
-	}
-
-
-	/**
-	 * Gets the medical history.
-	 * 
-	 * @return the medical history
-	 */
-	public MedicalHistory getMedicalHistory()
-	{
-		return this.myMedHistory;
-	}
-
-
-	/**
-	 * Sets the health history.
-	 * 
-	 * @param HHistory
-	 *            the new health history
-	 */
-	public void setHealthHistory(HealthHistory HHistory)
-	{
-		this.myHealthHistory = HHistory;
-	}
-
-
-	/**
-	 * View health history.
-	 */
-	public void ViewHealthHistory()
-	{
-		// TODO Implement Method
-	}
-
-
-	/**
-	 * View invoice.
-	 */
-	public void ViewInvoice()
-	{
-		// TODO Implement Method
-	}
-
-
-	/**
-	 * Gets the health history.
-	 * 
-	 * @return the health history
-	 */
-	public HealthHistory getHealthHistory()
-	{
-		return myHealthHistory;
-	}
-
-
-	/**
-	 * Sets the appointment history.
-	 * 
-	 * @param appointmentHistory
-	 *            the new appointment history
-	 */
-	public void setAppointmentHistory(ArrayList<Appointment> appointmentHistory)
-	{
-		AppointmentHistory = appointmentHistory;
-	}
-
-
-	/**
-	 * Gets the appointment history.
-	 * 
-	 * @return the appointment history
-	 */
-	public ArrayList<Appointment> getAppointmentHistory()
-	{
-		return AppointmentHistory;
-	}
-	
 }
