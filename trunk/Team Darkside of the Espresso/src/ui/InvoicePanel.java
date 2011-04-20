83641720eb61dcdbb50bf1d6c318a75c8f8c36ad
ptynan123 <ptynan123@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-04-20 19:21:09 +0000

    Completed Invoice Panel UI Class

--- trunk/Team Darkside of the Espresso/src/ui/InvoicePanel.java
+++ trunk/Team Darkside of the Espresso/src/ui/InvoicePanel.java
@@ -6,6 +6,9 @@ package ui;
 
 import javax.swing.JPanel;
 import appointment.Appointment;
+import javax.swing.JLabel;
+import java.awt.Font;
+import javax.swing.JButton;
 
 /**
  * The InvoicePanel Class.
@@ -28,6 +31,49 @@ public class InvoicePanel extends JPanel {
      * Create the panel.
      */
     public InvoicePanel(Appointment app) {
+    	setLayout(null);
+    	
+    	JLabel lblAppointmentInvoice = new JLabel("Appointment Invoice");
+    	lblAppointmentInvoice.setFont(new Font("Lucida Grande", Font.BOLD, 16));
+    	lblAppointmentInvoice.setBounds(136, 17, 178, 26);
+    	add(lblAppointmentInvoice);
+    	
+    	JLabel lblLabs = new JLabel("Labs:");
+    	lblLabs.setBounds(80, 77, 61, 16);
+    	add(lblLabs);
+    	
+    	JLabel lblVaccines = new JLabel("Vaccines:");
+    	lblVaccines.setBounds(80, 105, 61, 16);
+    	add(lblVaccines);
+    	
+    	JLabel lblPrescriptions = new JLabel("Prescriptions:");
+    	lblPrescriptions.setBounds(80, 133, 92, 16);
+    	add(lblPrescriptions);
+    	
+    	JLabel lblLabcost = new JLabel("LabCost");
+    	lblLabcost.setBounds(285, 77, 61, 16);
+    	add(lblLabcost);
+    	
+    	JLabel lblVaccinecost = new JLabel("VaccineCost");
+    	lblVaccinecost.setBounds(285, 105, 61, 16);
+    	add(lblVaccinecost);
+    	
+    	JLabel lblPrescriptioncost = new JLabel("PrescriptionCost");
+    	lblPrescriptioncost.setBounds(285, 133, 61, 16);
+    	add(lblPrescriptioncost);
+    	
+    	JLabel lblTotal = new JLabel("Total");
+    	lblTotal.setFont(new Font("Lucida Grande", Font.BOLD, 13));
+    	lblTotal.setBounds(80, 224, 61, 16);
+    	add(lblTotal);
+    	
+    	JLabel lblTotalcost = new JLabel("TotalCost");
+    	lblTotalcost.setBounds(285, 224, 61, 16);
+    	add(lblTotalcost);
+    	
+    	JButton btnOkay = new JButton("Return");
+    	btnOkay.setBounds(166, 265, 117, 29);
+    	add(btnOkay);
 	// TODO Implement Method
     }
 
