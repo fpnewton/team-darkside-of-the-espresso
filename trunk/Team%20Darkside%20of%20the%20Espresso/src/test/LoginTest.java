689f3de44fa1dff8e35445ef4a0646ae684fe104
fpnewton90 <fpnewton90@edb38e8c-05d4-51d8-a7e8-07f77d3579c2>
2011-04-27 19:59:54 +0000

    Added test files

---
+++ trunk/Team%20Darkside%20of%20the%20Espresso/src/test/LoginTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.util.Date;
+
+import junit.framework.TestCase;
+import ui.LoginController;
+import users.GenderType;
+import users.Patient;
+import users.PatientInfo;
+import users.UserInfo;
+import database.SqlDatabase;
+
+
+public class LoginTest extends TestCase {
+	
+	LoginController login = new LoginController();
+	SqlDatabase db;
+	
+	public void testLogin() {
+		
+		try {
+			db = new SqlDatabase();
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		Patient testPat = new Patient("fnewton3","password",GenderType.Male,
+				new UserInfo("Fraser Newton","Some Address","999999999","","",new Date()), 
+				new PatientInfo(18, "BCBS", "pharmacy", null, null));
+		
+		db.canInsertUser(testPat);
+		
+		assertTrue(login.canLogin(testPat.getUsername(), "password"));
+	}
+
+}
---
+++ trunk/Team%20Darkside%20of%20the%20Espresso/src/test/ScheduleAppointmentTest.java
@@ -0,0 +1,41 @@
+package test;
+
+import java.sql.Time;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import junit.framework.Assert;
+import junit.framework.TestCase;
+import users.Doctor;
+import users.GenderType;
+import users.Patient;
+import users.PatientInfo;
+import users.UserInfo;
+
+
+
+public class ScheduleAppointmentTest extends TestCase {
+	
+	SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+	Doctor doctor;
+	
+	public void testAppointment(){
+		Patient testPat = new Patient("fnewton3","password",GenderType.Male,
+				new UserInfo("Fraser Newton","Some Address","999999999","","",new Date()), 
+				new PatientInfo(18, "BCBS", "pharmacy", null, null));
+		Date date = null;
+		try {
+			date = formatter.parse("01/01/2010");
+		} catch (ParseException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		testPat.ScheduleAppointment(date, new Time(11l), "symptoms", doctor);
+		
+		Assert.assertNotNull(testPat.getAppointmentHistory());
+		Assert.assertNotNull(doctor.getCurrentAppointments());
+		
+	}
+
+}
